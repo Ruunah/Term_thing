948dbae08a4dd2913976f14241ef48afb473f12a
Luna <lunarpb09@gmail.com>
2025-08-18 14:04:52 -0300

    modified:   utils/window/QTermEdit.py 	modified:   utils/window/command_sugestions.py

--- utils/window/command_sugestions.py
+++ utils/window/command_sugestions.py
@@ -1,10 +1,15 @@
 from PySide6.QtGui import QColor, QPainter, QFontMetrics
 from PySide6.QtCore import QPoint
 from utils.window.loads import defaults, load_sets, hex_to_rgb
+from commands import command_registry
 
 self = load_sets(defaults)
 
-class GhostTextMixin:
+class GhostTextMixin(parent=""):
     def __init__(self):
         self._ghost_text = ""
-        self._ghost_color = QColor(hex_to_rgb(self["Foreground"]))
+        self._ghost_color = QColor(hex_to_rgb(self["Foreground"], "0.5"))
+        self.parent = parent
+
+    def check(self):
+        if self.parent.command 
